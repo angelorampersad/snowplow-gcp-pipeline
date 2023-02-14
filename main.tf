3c414ef3fe6b94b52729dc489ca087d67e3602c6
Angelo Rampersad <angelorampersad94@gmail.com>
2023-02-14 22:01:02 +0100

    reinstate

--- main.tf
+++ main.tf
@@ -1,181 +1,181 @@
-# locals {
-#   custom_iglu_resolvers = [
-#     {
-#       name            = "Iglu Server"
-#       priority        = 0
-#       uri             = "${var.iglu_server_dns_name}/api"
-#       api_key         = var.iglu_super_api_key
-#       vendor_prefixes = []
-#     }
-#   ]
-# }
+locals {
+  custom_iglu_resolvers = [
+    {
+      name            = "Iglu Server"
+      priority        = 0
+      uri             = "${var.iglu_server_dns_name}/api"
+      api_key         = var.iglu_super_api_key
+      vendor_prefixes = []
+    }
+  ]
+}
 
-# provider "google" {
-#   project = var.project_id
-#   region  = var.region
-# }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
 
-# # 1. Deploy PubSub Topics
-# module "raw_topic" {
-#   source  = "snowplow-devops/pubsub-topic/google"
-#   version = "0.1.0"
+# 1. Deploy PubSub Topics
+module "raw_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
 
-#   name = "${var.prefix}-raw-topic"
+  name = "${var.prefix}-raw-topic"
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# module "bad_1_topic" {
-#   source  = "snowplow-devops/pubsub-topic/google"
-#   version = "0.1.0"
+module "bad_1_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
 
-#   name = "${var.prefix}-bad-1-topic"
+  name = "${var.prefix}-bad-1-topic"
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# module "enriched_topic" {
-#   source  = "snowplow-devops/pubsub-topic/google"
-#   version = "0.1.0"
+module "enriched_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
 
-#   name = "${var.prefix}-enriched-topic"
+  name = "${var.prefix}-enriched-topic"
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# # 2. Deploy Collector stack
-# module "collector_pubsub" {
-#   source  = "snowplow-devops/collector-pubsub-ce/google"
-#   version = "0.2.2"
+# 2. Deploy Collector stack
+module "collector_pubsub" {
+  source  = "snowplow-devops/collector-pubsub-ce/google"
+  version = "0.2.2"
 
-#   name = "${var.prefix}-collector-server"
+  name = "${var.prefix}-collector-server"
 
-#   network    = var.network
-#   subnetwork = var.subnetwork
-#   region     = var.region
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
 
-#   ssh_ip_allowlist = var.ssh_ip_allowlist
-#   ssh_key_pairs    = var.ssh_key_pairs
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
 
-#   topic_project_id = var.project_id
-#   good_topic_name  = module.raw_topic.name
-#   bad_topic_name   = module.bad_1_topic.name
+  topic_project_id = var.project_id
+  good_topic_name  = module.raw_topic.name
+  bad_topic_name   = module.bad_1_topic.name
 
-#   telemetry_enabled = var.telemetry_enabled
-#   user_provided_id  = var.user_provided_id
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
 
-#   associate_public_ip_address = false
+  associate_public_ip_address = false
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# module "collector_lb" {
-#   source  = "snowplow-devops/lb/google"
-#   version = "0.1.0"
+module "collector_lb" {
+  source  = "snowplow-devops/lb/google"
+  version = "0.1.0"
 
-#   name = "${var.prefix}-collector-lb"
+  name = "${var.prefix}-collector-lb"
 
-#   instance_group_named_port_http = module.collector_pubsub.named_port_http
-#   instance_group_url             = module.collector_pubsub.instance_group_url
-#   health_check_self_link         = module.collector_pubsub.health_check_self_link
+  instance_group_named_port_http = module.collector_pubsub.named_port_http
+  instance_group_url             = module.collector_pubsub.instance_group_url
+  health_check_self_link         = module.collector_pubsub.health_check_self_link
 
-#   ssl_certificate_enabled = var.ssl_information.enabled
-#   ssl_certificate_id      = var.ssl_information.certificate_id
-# }
+  ssl_certificate_enabled = var.ssl_information.enabled
+  ssl_certificate_id      = var.ssl_information.certificate_id
+}
 
-# # 3. Deploy Enrichment
-# module "enrich_pubsub" {
-#   source  = "snowplow-devops/enrich-pubsub-ce/google"
-#   version = "0.1.2"
+# 3. Deploy Enrichment
+module "enrich_pubsub" {
+  source  = "snowplow-devops/enrich-pubsub-ce/google"
+  version = "0.1.2"
 
-#   name = "${var.prefix}-enrich-server"
+  name = "${var.prefix}-enrich-server"
 
-#   network    = var.network
-#   subnetwork = var.subnetwork
-#   region     = var.region
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
 
-#   ssh_ip_allowlist = var.ssh_ip_allowlist
-#   ssh_key_pairs    = var.ssh_key_pairs
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
 
-#   raw_topic_name = module.raw_topic.name
-#   good_topic_id  = module.enriched_topic.id
-#   bad_topic_id   = module.bad_1_topic.id
+  raw_topic_name = module.raw_topic.name
+  good_topic_id  = module.enriched_topic.id
+  bad_topic_id   = module.bad_1_topic.id
 
-#   # Linking in the custom Iglu Server here
-#   custom_iglu_resolvers = local.custom_iglu_resolvers
+  # Linking in the custom Iglu Server here
+  custom_iglu_resolvers = local.custom_iglu_resolvers
 
-#   telemetry_enabled = var.telemetry_enabled
-#   user_provided_id  = var.user_provided_id
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
 
-#   associate_public_ip_address = false
+  associate_public_ip_address = false
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# # 5. Deploy BigQuery Loader
-# module "bad_rows_topic" {
-#   source  = "snowplow-devops/pubsub-topic/google"
-#   version = "0.1.0"
+# 5. Deploy BigQuery Loader
+module "bad_rows_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
 
-#   count = var.bigquery_db_enabled ? 1 : 0
+  count = var.bigquery_db_enabled ? 1 : 0
 
-#   name = "${var.prefix}-bq-bad-rows-topic"
+  name = "${var.prefix}-bq-bad-rows-topic"
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# resource "google_bigquery_dataset" "bigquery_db" {
-#   count = var.bigquery_db_enabled ? 1 : 0
+resource "google_bigquery_dataset" "bigquery_db" {
+  count = var.bigquery_db_enabled ? 1 : 0
 
-#   dataset_id = replace("${var.prefix}_pipeline_db", "-", "_")
-#   location   = var.region
+  dataset_id = replace("${var.prefix}_pipeline_db", "-", "_")
+  location   = var.region
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# resource "google_storage_bucket" "bq_loader_dead_letter_bucket" {
-#   count = var.bigquery_db_enabled && var.bigquery_loader_dead_letter_bucket_deploy ? 1 : 0
+resource "google_storage_bucket" "bq_loader_dead_letter_bucket" {
+  count = var.bigquery_db_enabled && var.bigquery_loader_dead_letter_bucket_deploy ? 1 : 0
 
-#   name          = join("-", [var.bigquery_loader_dead_letter_bucket_name, var.project_id])
-#   location      = var.region
-#   force_destroy = true
+  name          = join("-", [var.bigquery_loader_dead_letter_bucket_name, var.project_id])
+  location      = var.region
+  force_destroy = true
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
 
-# locals {
-#   bq_loader_dead_letter_bucket_name = coalesce(
-#     join("", google_storage_bucket.bq_loader_dead_letter_bucket.*.name),
-#     var.bigquery_loader_dead_letter_bucket_name,
-#   )
-# }
+locals {
+  bq_loader_dead_letter_bucket_name = coalesce(
+    join("", google_storage_bucket.bq_loader_dead_letter_bucket.*.name),
+    var.bigquery_loader_dead_letter_bucket_name,
+  )
+}
 
-# module "bigquery_loader" {
-#   source  = "snowplow-devops/bigquery-loader-pubsub-ce/google"
-#   version = "0.1.0"
+module "bigquery_loader" {
+  source  = "snowplow-devops/bigquery-loader-pubsub-ce/google"
+  version = "0.1.0"
 
-#   count = var.bigquery_db_enabled ? 1 : 0
+  count = var.bigquery_db_enabled ? 1 : 0
 
-#   name = "${var.prefix}-bq-loader-server"
+  name = "${var.prefix}-bq-loader-server"
 
-#   network    = var.network
-#   subnetwork = var.subnetwork
-#   region     = var.region
-#   project_id = var.project_id
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
+  project_id = var.project_id
 
-#   ssh_ip_allowlist = var.ssh_ip_allowlist
-#   ssh_key_pairs    = var.ssh_key_pairs
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
 
-#   input_topic_name            = module.enriched_topic.name
-#   bad_rows_topic_name         = join("", module.bad_rows_topic.*.name)
-#   gcs_dead_letter_bucket_name = local.bq_loader_dead_letter_bucket_name
-#   bigquery_dataset_id         = join("", google_bigquery_dataset.bigquery_db.*.dataset_id)
+  input_topic_name            = module.enriched_topic.name
+  bad_rows_topic_name         = join("", module.bad_rows_topic.*.name)
+  gcs_dead_letter_bucket_name = local.bq_loader_dead_letter_bucket_name
+  bigquery_dataset_id         = join("", google_bigquery_dataset.bigquery_db.*.dataset_id)
 
-#   # Linking in the custom Iglu Server here
-#   custom_iglu_resolvers = local.custom_iglu_resolvers
+  # Linking in the custom Iglu Server here
+  custom_iglu_resolvers = local.custom_iglu_resolvers
 
-#   telemetry_enabled = var.telemetry_enabled
-#   user_provided_id  = var.user_provided_id
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
 
-#   labels = var.labels
-# }
+  labels = var.labels
+}
